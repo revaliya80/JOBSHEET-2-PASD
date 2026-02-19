e6a7a7e97069a86a1d4639a533756a12e5ad4edf
Refaliya-Vinandha <revaliya80@gmail.com>
2026-02-19 14:00:54 +0700

    Percobaan 1

---
+++ mahasiswa24.java
@@ -0,0 +1,34 @@
+public class mahasiswa24 {
+
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+}
