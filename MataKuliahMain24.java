00fb2d538418cc433381840fe04db26744d16122
Refaliya-Vinandha <revaliya80@gmail.com>
2026-02-20 09:18:01 +0700

    tugas 1

--- MataKuliahMain24.java
+++ MataKuliahMain24.java
@@ -5,6 +5,7 @@ public class MataKuliahMain24 {
         mk1.nama = "Refaliya Vinandha";
         mk1.sks = 3;
         mk1.jumlahjam = 6;
+        System.out.println();
 
         mk1.tampilinformasi();
         mk1.ubahSKS(4);
